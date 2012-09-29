5ac8dd2f56a4d31787aa3e3bca64eb2d7f08b3ae
Salja <salja2012@hotmail.de>
2012-09-29 13:05:21 +0200

    Some Typos
    Signed-off-by: Salja <salja2012@hotmail.de>

--- src/game/World.cpp
+++ src/game/World.cpp
@@ -736,7 +736,7 @@ void World::LoadConfigSettings(bool reload)
 
     setConfig(CONFIG_BOOL_PET_UNSUMMON_AT_MOUNT,      "PetUnsummonAtMount", true);
 
-    setConfig(CONFIG_BOOL_WARDEN_KICK,                 "Warden.Kick", false);
+    setConfig(CONFIG_BOOL_WARDEN_KICK,                "Warden.Kick", false);
 
     m_relocation_ai_notify_delay = sConfig.GetIntDefault("Visibility.AIRelocationNotifyDelay", 1000u);
     m_relocation_lower_limit_sq  = pow(sConfig.GetFloatDefault("Visibility.RelocationLowerLimit",10), 2);
