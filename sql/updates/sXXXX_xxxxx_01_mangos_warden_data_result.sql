1a95261a68acdab49e30ef60b57939e9c267b989
Salja <salja2012@hotmail.de>
2012-09-30 13:50:12 +0200

    Update base SQL
    Signed-off-by: Salja <salja2012@hotmail.de>
    
    Conflicts:
    	sql/mangos.sql
    	sql/realmd.sql

---
+++ sql/updates/sXXXX_xxxxx_01_mangos_warden_data_result.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS `warden_data_result`;
+
+CREATE TABLE `warden_data_result` (
+  `id` int(4) NOT NULL AUTO_INCREMENT,
+  `check` int(3) DEFAULT NULL,
+  `data` tinytext,
+  `str` tinytext,
+  `address` int(8) DEFAULT NULL,
+  `length` int(2) DEFAULT NULL,
+  `result` tinytext,
+  `comment` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1004 DEFAULT CHARSET=utf8;
